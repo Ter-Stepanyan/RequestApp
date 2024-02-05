f4bd6fb769177ef455d0f97cb8339582939ebb2f
Ter-Stepanyan <artak.ter-stepanyan@team.gg>
2024-02-05 16:58:33 +0400

    PersonTableView created using XIB.

---
+++ Request/CustomTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  CustomTableViewCell.swift
+//  Request
+//
+//  Created by Artak Ter-Stepanyan on 31.01.24.
+//
+
+import Foundation
---
+++ Request/PersonTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  PersonTableViewCell.swift
+//  Request
+//
+//  Created by Artak Ter-Stepanyan on 02.02.24.
+//
+
+import Foundation
